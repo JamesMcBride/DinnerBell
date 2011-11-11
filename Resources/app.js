feafeb904706b02a1a5fb88b5ddf39f0e99ce321
Developer <developer@ip-144-38-95-238.dixie.edu>
2011-11-11 15:17:07 -0700

    fixed bell ring

--- Resources/app.js
+++ Resources/app.js
@@ -112,33 +112,106 @@ var ringbell = Titanium.UI.createButton({
  {
 
 	var w = Titanium.UI.createWindow({
-		backgroundColor:'#336699'
-	});
-	// TODO: list contacts that will receive text
-	
-	// create a button to close window
-	var ring = Titanium.UI.createButton({
-		title:'Ring Bell',
-		height:30,
-		width:150,
-		top:20
-	});
-	w.add(ring);
-	ring.addEventListener('click', function()
-	{
-		ringBell();
+		backgroundColor:'#336699',
+		backgroundImage:'/images/table_cloth.png'
 	});
 	
-	var b = Titanium.UI.createButton({
-		title:'Close',
-		height:30,
-		width:150
+	var imageView = Titanium.UI.createImageView({
+	image:'/images/bell.png',
+	width:'auto',
+	height:'auto',
+	top: 'auto' 
 	});
-	w.add(b);
-	b.addEventListener('click', function()
+	
+	imageView.addEventListener('load', function()
 	{
-		w.close();
+			var t1 = Ti.UI.create3DMatrix();
+	t1 = t1.translate(0,0,1000);
+	t1.m34 = 1.0/-90;
+	var a1 = Titanium.UI.createAnimation();
+	a1.transform = t1;
+	a1.duration = 1000;
+	a1.repeat = 1;
+	imageView.animate(a1);
+
+	a1.addEventListener('complete', function(e)
+	{
+			t1 = t1.rotate(180,1,1,0);
+			t1 = t1.scale(2.0,2.0,2.0);
+			t1.m34 = 1.0/-1500;
+			var a1 = Titanium.UI.createAnimation();
+			a1.transform = t1;
+			a1.duration = 1000;
+			a1.repeat = 1;
+			a1.autoreverse = true;
+			imageView.animate(a1);
+
+			a1.addEventListener('complete', function(e)
+			{
+					t1 = t1.rotate(180,0,1,1);
+					t1 = t1.scale(3.0,3.0,3.0);
+					t1.m34 = 1.0/-3000;
+					var a3 = Titanium.UI.createAnimation();
+					a3.transform = t1;
+					a3.duration = 1000;
+					a3.repeat = 1;
+					a3.addEventListener('complete',function()
+					{
+						imageView.animate({transform:Ti.UI.create3DMatrix(),duration:500});
+					});
+					imageView.animate(a3);
+			});
+
+			var label5 = Titanium.UI.createLabel({
+			color:'#999',
+			text:'Guests Notified',
+			font:{fontSize:20,fontFamily:'Helvetica Neue'},
+			textAlign:'center',
+			color: '#09B017',
+			width:'auto',
+			top: 330,
+			topMost: true
+		});
+		
+		w.add(label5);
+
+
 	});
+	
+	
+	
+	});
+	
+	w.add(imageView);
+	
+	var close = Titanium.UI.createButton({
+	width : 25,
+	height: 25,
+	top : 35,
+	left: 40,
+	backgroundImage:'/images/x_close.png'
+})	
+
+close.addEventListener('click', function()
+{
+	w.close();
+});
+
+w.add(close);
+
+
+	
+	var sound = Titanium.Media.createSound();
+	sound.url='/sounds/bell.mp3'; // For testing #1913
+
+
+	ringBell();
+	
+	sound.play();
+	
+	
+
+
 
 	w.open();
 });
