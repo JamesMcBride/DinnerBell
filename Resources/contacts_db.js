faa2f292423961470cf90796036822da1af8fdd1
Developer <developer@ip-144-38-95-35.dixie.edu>
2011-11-11 10:53:53 -0700

    Sucky-suck

--- Resources/contacts_db.js
+++ Resources/contacts_db.js
@@ -10,35 +10,12 @@ if (android) {
 	activityIndicator.show();
 }
 
-var paidSwitch = Titanium.UI.createSwitch({
-  top:240,
-  left:50,
-  value:false,
-  visible:false
-});
-win.add(paidSwitch);
-var paidCheckbox = Titanium.UI.createImageView({
-  image:'images/unchecked.png',
-  width:30,
-  height:30,
-  top:240,
-  left:50
-});
-paidCheckbox.addEventListener('click',function(e) {
-  if(paidSwitch.value==false) {
-    paidSwitch.value = true;
-    paidCheckbox.image = 'images/checked.png';
-  } else {
-    paidSwitch = false;
-    paidCheckbox.image = 'images/unchecked.png';
-  }
-});
-win.add(paidCheckbox);
 
 var makeTable = function() {
 	var people = Titanium.Contacts.getAllPeople();
 	var rows = [];
 	for (var i = 0; i < people.length; i++) {
+		
 		Ti.API.info("People object is: "+people[i]);
 		var title = people[i].fullName;
 		if (!title || title.length === 0) {
@@ -47,13 +24,12 @@ var makeTable = function() {
 		rows[i] = Ti.UI.createTableViewRow({
 			title: title,
 			person:people[i],
-			hasChild:false
+			hasChild:false,
+			
 		});
 		
-		
 		rows[i].addEventListener('click', function(e) {
-			
-            var selected = true;
+			Ti.UI.TableViewRow.rows[i].hasCheck =true;
 		});
 	}
 	return rows;
